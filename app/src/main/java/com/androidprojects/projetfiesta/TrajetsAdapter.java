3fd128996818bf9ade93cafca674c60052a9ba12
ntscharner <nico.tscharner@gmail.com>
2016-08-31 18:48:40 +0200

    Affiche_trajets_2
    Page d'affichage des trajets terminées
    A tester avec des événements qui ont des trajets
    (je n'ai pas encore gérer le fait qu'un événement n'a pas de trajet)

--- app/src/main/java/com/androidprojects/projetfiesta/TrajetsAdapter.java
+++ app/src/main/java/com/androidprojects/projetfiesta/TrajetsAdapter.java
@@ -2,19 +2,27 @@ package com.androidprojects.projetfiesta;
 
 import android.content.Context;
 import android.content.Intent;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import com.projetfiesta.backend.trajetApi.model.Trajet;
 
+import com.projetfiesta.backend.evenementApi.model.Evenement;
+import com.projetfiesta.backend.messageApi.model.Message;
+import com.projetfiesta.backend.trajetApi.model.Trajet;
+import com.projetfiesta.backend.utilisateurApi.model.Utilisateur;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 /**
- * Created by NTS on 28.08.2016.
+ * ArrayAdapter destiné à l'affichage des trajets
+ * Créé par NTS
  */
-public class TrajetsAdapter extends ArrayAdapter<Trajet> {
+public class TrajetsAdapter extends ArrayAdapter<Trajet> implements OnTaskCompleted {
 
     public TrajetsAdapter(Context context, List<Trajet> trajets) {
         super(context, 0, trajets);
@@ -23,27 +31,62 @@ public class TrajetsAdapter extends ArrayAdapter<Trajet> {
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
 
+        // On assigne le layout "row_trajets" créé pour afficher les trajets comme prévu
         if(convertView == null){
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.row_trajets,parent, false);
         }
 
+
         TrajetViewHolder viewHolder = (TrajetViewHolder) convertView.getTag();
         if(viewHolder == null){
             viewHolder = new TrajetViewHolder();
+            viewHolder.trajetChauffeur = (TextView) convertView.findViewById(R.id.chauffeur);
             viewHolder.trajetDestination = (TextView) convertView.findViewById(R.id.destination);
             viewHolder.trajetNbPlaces = (TextView) convertView.findViewById(R.id.nbPlaces);
             viewHolder.trajetDepart = (TextView) convertView.findViewById(R.id.depart);
             convertView.setTag(viewHolder);
         }
 
-        //getItem(position) va récupérer l'item [position] de la List<Trajet> trajets
+        // pour chaque trajet...
         Trajet trajet = getItem(position);
 
-        //il ne reste plus qu'à remplir notre vue
-        viewHolder.trajetDestination.setText(trajet.getDestination());
-        viewHolder.trajetNbPlaces.setText(trajet.getNombrePlaces());
-        viewHolder.trajetDepart.setText(trajet.getHeureDepart());
+        // .. on récupère le nom du chauffeur
+        List <Utilisateur> chauffeurs = new ArrayList<Utilisateur>();
+        try {
+            chauffeurs = new EndpointsAsyncTaskUtilisateur(trajet.getConducteurId(), this ).execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        Utilisateur chauffeur = chauffeurs.get(0);
+        String Chauffeur = "<b>"+chauffeur.getNom()+"</b>";
 
+        // ...sa destination
+        String Destination = "Destination "+"<b>"+trajet.getDestination()+"</b>";
+
+        // ... le nomnre de places disponibles
+        String NbPlaces;
+        if (trajet.getNombrePlaces()<2)
+        {
+            NbPlaces = "<b>"+String.valueOf(trajet.getNombrePlaces())+" place"+"</b>";
+        }
+        else
+        {
+            NbPlaces = "<b>"+String.valueOf(trajet.getNombrePlaces())+" places"+"</b>";
+        }
+
+        // ... et l'heure de départ
+        String Depart = "Départ "+"<b>"+"~"+trajet.getHeureDepart()+"</b>";
+
+        // ... pour finalement afficher tout le contenu
+        viewHolder.trajetChauffeur.setText(Html.fromHtml(Chauffeur));
+        viewHolder.trajetDestination.setText(Html.fromHtml(Destination));
+        viewHolder.trajetNbPlaces.setText(Html.fromHtml(NbPlaces));
+        viewHolder.trajetDepart.setText(Html.fromHtml(Depart));
+
+
+        // En cliquant sur la destination, on affiche le trajet concerné
         viewHolder.trajetDestination.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,9 +101,30 @@ public class TrajetsAdapter extends ArrayAdapter<Trajet> {
     }
 
     private class TrajetViewHolder{
+        public TextView trajetChauffeur;
         public TextView trajetDestination;
         public TextView trajetNbPlaces;
         public TextView trajetDepart;
+    }
+
+    @Override
+    public void updateListViewTrajet(List<Trajet> trajets) {
+
+    }
+
+    @Override
+    public void updateListViewUtilisateur(List<Utilisateur> utilisateurs) {
+
+    }
+
+    @Override
+    public void updateListViewEvenement(List<Evenement> evenements) {
+
+    }
+
+    @Override
+    public void updateListViewMessage(List<Message> messages) {
+
     }
 }
 

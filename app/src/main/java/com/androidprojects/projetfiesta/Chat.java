d2124bff472e6be490420b7b96d5f9f1e2120a72
Cyril <cyril.zufferey@outlook.com>
2016-09-08 21:00:48 +0200

    Correction si nom de ville trop long.

--- app/src/main/java/com/androidprojects/projetfiesta/Chat.java
+++ app/src/main/java/com/androidprojects/projetfiesta/Chat.java
@@ -1,5 +1,6 @@
 package com.androidprojects.projetfiesta;
 
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
@@ -14,6 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -62,20 +64,8 @@ public class Chat  extends AppCompatActivity implements OnTaskCompleted{
     private Message message;
     private List<Message> messages = new ArrayList<>();
 
-    //Bouton envoyer
-    private ImageButton envoyer;
+    private Button btnCorrection;
 
-    //numberPicker pour changer le nombre de places disponibles
-    private NumberPicker editNbPlaces;
-
-    //EditText pour modifier l'heure de départ
-    private EditText editHeure;
-
-    //Bouton pour valider le changement de nombre de place
-    private ImageButton btn_updateNbP;
-
-    //Bouton pour valider le changement d'heure de départ
-    private ImageButton btn_updateHeure;
 
     // Variable Pattern pour contrôler le format de l'heure entré par l'utilisateur
     private final String heurePattern = "([01]?[0-9]|2[0-3]):[0-5][0-9]";
@@ -91,18 +81,6 @@ public class Chat  extends AppCompatActivity implements OnTaskCompleted{
         // Permet de ne pas démarrer le keyboard automatiquement au lancement de l'activité
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
-        // L'édition du nombre de place est premièrement inaccessible
-        editNbPlaces = (NumberPicker) findViewById(R.id.numberPicker_places);
-        editNbPlaces.setVisibility(View.GONE);
-        btn_updateNbP = (ImageButton) findViewById(R.id.btnUpdateNbP);
-        btn_updateNbP.setVisibility(View.GONE);
-
-        // L'édition de l'heure est également inaccessible dans un premier temps
-        editHeure = (EditText) findViewById(R.id.editText_HeureD);
-        editHeure.setVisibility(View.GONE);
-        btn_updateHeure =(ImageButton) findViewById(R.id.btnUpdateH);
-        btn_updateHeure.setVisibility(View.GONE);
-
 
         //Récupération du trajet transmis par la vue précédente
         Intent intent = getIntent();
@@ -116,6 +94,19 @@ public class Chat  extends AppCompatActivity implements OnTaskCompleted{
             e.printStackTrace();
         }
         trajet = trajets.get(0);
+
+
+        btnCorrection = (Button) findViewById(R.id.buttonCorrection);
+        btnCorrection.setVisibility(View.GONE);
+
+        SharedPreferences settings = getSharedPreferences("prefs",0);
+        Long idUtilisateur = settings.getLong("idUtilisateur",0);
+
+        Long idCond = trajet.getConducteurId();
+
+        if(idCond.longValue() == idUtilisateur.longValue()){
+            btnCorrection.setVisibility(View.VISIBLE);
+        }
 
         //Affichage des informations sur le trajet
         tvDepart = (TextView) findViewById(R.id.tvDepart);
@@ -167,48 +158,17 @@ public class Chat  extends AppCompatActivity implements OnTaskCompleted{
         tvNomConducteur.setText(conducteur.getPrenom()+" "+conducteur.getNom().charAt(0)+".");
 
 
-        //Bouton d'envoi du  message
-        envoyer = (ImageButton) findViewById(R.id.btnEnvoyer);
-
-        // Le pickNumber pour changer le nombre de places disponibles
-        // et l'EditText pour modifier l'heure de départ
-        // doivent être accessibles uniquement si le chaffeur est l'utilisateur en cours
-        SharedPreferences settings = getSharedPreferences("prefs",0);
-        Long idUtilisateur = settings.getLong("idUtilisateur",0);
-
-        if (String.valueOf(idUtilisateur).equals(String.valueOf(conducteur.getId())))
-        {
-            tvNbPlace.setVisibility(View.GONE);
-            tvDepart.setVisibility(View.GONE);
-
-            btn_updateNbP.setVisibility(View.VISIBLE);
-            btn_updateHeure.setVisibility(View.VISIBLE);
-
-            editNbPlaces.setVisibility(View.VISIBLE);
-            editHeure.setVisibility(View.VISIBLE);
-
-            int whiteColorValue = Color.WHITE;
-            setNumberPickerTextColor(editNbPlaces, whiteColorValue);
-            editNbPlaces.setMaxValue(20);
-            editNbPlaces.setMinValue(0);
-            editNbPlaces.setValue(trajet.getNombrePlaces());
-            editNbPlaces.setWrapSelectorWheel(false);
-
-            editHeure.setHint(trajet.getHeureDepart());
-
-        }
     }
 
-    public void insert (View v){
+    public void insert (View v) {
         EditText texte = (EditText) findViewById(R.id.etMessage);
 
         //Si le texte n'est pas vide, on attribue le texte au message
-        if (texte==null || texte.getText().toString().trim().equals("")) {
+        if (texte == null || texte.getText().toString().trim().equals("")) {
 
             return;
-        }
-        else {
-            message=new Message();
+        } else {
+            message = new Message();
             message.setTexte(texte.getText().toString());
         }
         //Ajout de l'heure
@@ -218,14 +178,14 @@ public class Chat  extends AppCompatActivity implements OnTaskCompleted{
         message.setTrajetId(trajetId);
 
         //Récupération et ajout de l'id de l'utilisateur
-        SharedPreferences settings = getSharedPreferences("prefs",0);
-        Long idUtilisateur = settings.getLong("idUtilisateur",0);
+        SharedPreferences settings = getSharedPreferences("prefs", 0);
+        Long idUtilisateur = settings.getLong("idUtilisateur", 0);
         message.setUtilisateurId(idUtilisateur);
 
         //Envoi du message et rafraîchissement de l'activité
         new EndpointsAsyncTaskMessage(0, message, this).execute();
         Intent intent = new Intent(getBaseContext(), Chat.class);
-        intent.putExtra("trajetId",trajetId);
+        intent.putExtra("trajetId", trajetId);
         intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
         texte.setText("");
         this.onResume();
@@ -233,34 +193,8 @@ public class Chat  extends AppCompatActivity implements OnTaskCompleted{
 
     }
 
-    // Modification du nombre de places disponible pour un trajet donné
-    public void update_nbPlaces (View v){
-        trajet.setNombrePlaces(editNbPlaces.getValue());
-
-        new EndpointsAsyncTaskTrajet(1, trajet, this).execute();
-        Toast.makeText(Chat.this,
-                "Le nombre de places disponibles a été défini à "+editNbPlaces.getValue(), Toast.LENGTH_LONG).show();
-
-    }
-
-    // Modification de l'heure de départ pour un trajet donné
-    public void update_heureD (View v){
 
 
-        if (editHeure.getText().toString().matches(heurePattern))
-        {
-            trajet.setHeureDepart(editHeure.getText().toString());
-
-            new EndpointsAsyncTaskTrajet(1, trajet, this).execute();
-            Toast.makeText(Chat.this,
-                    "L'heure de départ a été défini à "+editHeure.getText().toString(), Toast.LENGTH_LONG).show();
-        }
-        else
-        {
-            Toast.makeText(getApplicationContext(), R.string.heure_invalide, Toast.LENGTH_SHORT).show();
-            return;
-        }
-    }
 
     @Override
     public void updateListViewTrajet(List<Trajet> trajets) {
@@ -287,6 +221,19 @@ public class Chat  extends AppCompatActivity implements OnTaskCompleted{
         listView.setAdapter(adapter);
 
     }
+
+
+
+    public void changertTransport(View view) {
+
+        DialogFragment newFragment = new EditionTrajetDialogAlert();
+        Bundle args = new Bundle();
+        args.putLong("trajetId",trajetId);
+        newFragment.setArguments(args);
+        newFragment.show(getFragmentManager(), "edit");
+
+    }
+
 
     private class MessageComparator implements Comparator<Message> {
         public int compare(Message messageA, Message messageB) {
---
+++ app/src/main/java/com/androidprojects/projetfiesta/EditionTrajetDialogAlert.java
@@ -0,0 +1,145 @@
+package com.androidprojects.projetfiesta;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.projetfiesta.backend.evenementApi.model.Evenement;
+import com.projetfiesta.backend.messageApi.model.Message;
+import com.projetfiesta.backend.trajetApi.model.Trajet;
+import com.projetfiesta.backend.utilisateurApi.model.Utilisateur;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by rilcy on 07.09.16.
+ */
+public class EditionTrajetDialogAlert extends DialogFragment implements OnTaskCompleted {
+
+    private Long trajetId;
+    private Activity activity;
+    private Trajet trajet;
+
+    private EditText nombrePlaces;
+    private EditText heureDepart;
+    LayoutInflater inflater;
+
+
+    // Variable Pattern pour contrôler le format de l'heure entré par l'utilisateur
+    private final String heurePattern = "([01]?[0-9]|2[0-3]):[0-5][0-9]";
+
+    @Override
+    public Dialog onCreateDialog(final Bundle savedInstanceState) {
+        trajetId = getArguments().getLong("trajetId");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        inflater = getActivity().getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.activityalert_editiontrajet, null))
+
+
+
+                .setPositiveButton(R.string.editer, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Correction du trajet
+                        // Récupération du trajet
+                        List<Trajet> trajets = new ArrayList<Trajet>();
+                        try {
+                            trajets = new EndpointsAsyncTaskTrajet(EditionTrajetDialogAlert.this, trajetId).execute().get();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } catch (ExecutionException e) {
+                            e.printStackTrace();
+                        }
+                        trajet = trajets.get(0);
+
+                        View content =  inflater.inflate(R.layout.activityalert_editiontrajet, null);
+
+                        nombrePlaces = (EditText) getDialog().findViewById(R.id.nbPlacesEntree);
+                        heureDepart = (EditText) getDialog().findViewById(R.id.heureEntree);
+
+                        update(content);
+                        refreshMyActivity();
+
+                      }
+                })
+                .setNegativeButton(R.string.annuler, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        EditionTrajetDialogAlert.this.getDialog().cancel();
+                    }
+                });
+
+        return builder.create();
+    }
+
+    private void refreshMyActivity() {
+        getActivity().finish();
+        Intent intent = getActivity().getIntent();
+        getActivity().overridePendingTransition(0, 0);
+        startActivity(getActivity().getIntent());
+        getActivity().overridePendingTransition(0, 0);
+    }
+
+
+    // Modification du nombre de places disponible pour un trajet donné
+    public void update_nbPlaces (View v){
+
+
+    }
+
+    // Modification de l'heure de départ pour un trajet donné
+    public void update (View v){
+
+        if(nombrePlaces.length()>0) {
+            int nb = Integer.parseInt(nombrePlaces.getText().toString());
+
+            trajet.setNombrePlaces(nb);
+        }
+
+
+        if (heureDepart.getText().toString().matches(heurePattern))
+        {
+            trajet.setHeureDepart(heureDepart.getText().toString());
+
+            new EndpointsAsyncTaskTrajet(1, trajet, this).execute();
+
+        }
+        else
+        {
+            Toast.makeText(EditionTrajetDialogAlert.this.getActivity(), R.string.heure_invalide, Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    @Override
+    public void updateListViewTrajet(List<Trajet> trajets) {
+
+    }
+
+    @Override
+    public void updateListViewUtilisateur(List<Utilisateur> utilisateurs) {
+
+    }
+
+    @Override
+    public void updateListViewEvenement(List<Evenement> evenements) {
+
+    }
+
+    @Override
+    public void updateListViewMessage(List<Message> messages) {
+
+    }
+}
